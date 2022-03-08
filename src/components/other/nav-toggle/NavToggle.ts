b0d970fbab543964beae41111b19b43679abc672
Iga Gruszczyńska <iga.gruszczynska@owlsdepartment.com>
2022-03-08 18:23:43 +0100

    feat(nav-toggle): add nav toggle component

---
+++ src/components/other/nav-toggle/NavToggle.ts
@@ -0,0 +1,151 @@
+import { bind, Component, dataParam, Logger, register } from 'ovee.js';
+
+type toggleEvent = CustomEvent<boolean>;
+
+const logger = new Logger('NavToggle');
+const DEFAULT_NAV_NAME = 'menu';
+
+@register('nav-toggle')
+export class NavToggle extends Component {
+	nav: HTMLElement;
+	animStarted: boolean;
+
+	@dataParam('navToggle')
+	navName: string;
+
+	@dataParam('nav')
+	navSelector: string;
+
+	@dataParam('showImmediately')
+	protected _showImmediately: string;
+
+	@dataParam('hideImmediately')
+	protected _hideImmediately: string;
+
+	isOpen = false;
+
+	get html() {
+		return document.documentElement;
+	}
+
+	get showImmediately() {
+		return this._showImmediately === 'true';
+	}
+
+	get hideImmediately() {
+		return this._hideImmediately === 'true';
+	}
+
+	init() {
+		if (!this.navName) {
+			this.navName = DEFAULT_NAV_NAME;
+		}
+
+		if (!this.navSelector) {
+			logger.error(`Attribute 'data-nav' with value is required`);
+			return;
+		}
+
+		const nav = document.querySelector<HTMLElement>(this.navSelector);
+
+		if (!nav) {
+			logger.error(`Could not find nav with selector: '${this.navSelector}'`);
+			return;
+		}
+
+		this.nav = nav;
+
+		this.bind();
+	}
+
+	bind() {
+		this.$on('nav-toggle:show', this.onNavShow.bind(this));
+		this.$on('nav-toggle:hide', this.onNavHide.bind(this));
+		this.$on('transitionstart', this.nav, this.animStart);
+		this.$on('transitionend', this.nav, this.animEnd);
+	}
+
+	@bind('click')
+	onClick(e: Event) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		if (this.isOpen) {
+			this.hide(this.hideImmediately);
+		} else {
+			this.show(this.showImmediately);
+		}
+	}
+
+	onNavShow(e: toggleEvent) {
+		this.show(e.detail);
+	}
+
+	onNavHide(e: toggleEvent) {
+		this.hide(e.detail);
+	}
+
+	animStart() {
+		if (this.animStarted) {
+			return;
+		}
+
+		this.animStarted = true;
+
+		if (this.isOpen) {
+			this.html.classList.add(`${this.navName}-anim`);
+		} else {
+			this.html.classList.add(`${this.navName}-hide-anim`);
+		}
+	}
+
+	animEnd() {
+		if (!this.animStarted) {
+			return;
+		}
+
+		this.animStarted = false;
+
+		if (this.isOpen) {
+			this.html.classList.remove(`${this.navName}-anim`);
+		} else {
+			this.html.classList.remove(`${this.navName}-hide-anim`);
+		}
+	}
+
+	show(immediately = false) {
+		this.isOpen = true;
+
+		if (immediately) {
+			this.html.classList.add(`${this.navName}-show-immediately`);
+
+			requestAnimationFrame(() => {
+				this.html.classList.remove(`${this.navName}-show-immediately`);
+			});
+		}
+
+		this.html.classList.add(`${this.navName}-visible`);
+		this.$element.setAttribute('aria-expanded', 'true');
+
+		this.$emit('nav-toggle:visible', this.$element);
+		this.$emit(`${this.navName}-toggle:visible`, this.html);
+	}
+
+	hide(immediately = false) {
+		this.isOpen = false;
+
+		if (immediately) {
+			this.html.classList.add(`${this.navName}-hide-immediately`);
+
+			requestAnimationFrame(() => {
+				this.html.classList.remove(`${this.navName}-hide-immediately`);
+			});
+		}
+
+		this.html.classList.remove(`${this.navName}-visible`);
+		this.$element.setAttribute('aria-expanded', 'false');
+
+		this.$emit('nav-toggle:hidden', this.$element);
+		this.$emit(`${this.navName}-toggle:hidden`, this.html);
+	}
+}

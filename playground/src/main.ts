d92ab515142d05731e876ddd7051dfbda94fca81
Iga Gruszczyńska <iga.gruszczynska@owlsdepartment.com>
2022-04-13 11:46:46 +0200

    fix(vh-fix): changes after cr

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,8 +2,12 @@ import '~/styles/main.scss';
 
 import { App } from 'ovee.js';
 
+import components from './components';
+
 const root = document.getElementById('app');
-const app = new App({});
+const app = new App({
+	components,
+});
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 app.run(root!);

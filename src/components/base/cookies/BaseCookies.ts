9472e8822d2d7f96a00ff6c56603d4c0f978fdc0
Miłosz Mandowski <milosz.mandowski@owlsdepartment.com>
2022-05-30 16:53:35 +0200

    fix(base-cookies): adjust base-cookies to proper code standard

--- src/components/base/cookies/BaseCookies.ts
+++ src/components/base/cookies/BaseCookies.ts
@@ -1,55 +1,70 @@
-import { Component, register, bind, Logger } from 'ovee.js';
+import { bind, Component, dataParam, Logger, register } from 'ovee.js';
+
+export interface BaseCookiesOptions {
+	storageKey?: string;
+}
+
+export const BASE_COOKIES_DEFAULT_OPTIONS: BaseCookiesOptions = {
+	storageKey: 'cookies_accepted',
+};
 
 const logger = new Logger('BaseCookies');
 
 @register('base-cookies')
-export default class extends Component {
-	siteUrl: string | undefined;
+export class BaseCookies extends Component {
 	storageKey: string;
-	$element: any;
+	options: BaseCookiesOptions;
+
+	@dataParam('storageKey')
+	_storageKey?: string;
+
+	get cookiesAccepted(): boolean {
+		return localStorage.getItem(this.storageKey) === 'true';
+	}
 
 	init() {
-		this.siteUrl = document.body.dataset.baseHref;
-        this.statusCheck = this.statusCheck.bind(this);
-        this.storageKey = `${this.siteUrl}cookies_accepted`;
+		this.options = {
+			...BASE_COOKIES_DEFAULT_OPTIONS,
+			...this.$options,
+		};
+
+		const key = this._storageKey || this.options.storageKey;
+
+		if (!key) {
+			logger.error(
+				`No 'storageKey' option is provided via config nor via data param. This option is required`
+			);
+
+			return;
+		}
+
+		this.storageKey = key;
 
 		this.statusCheck();
-		this.bind();
-    }
-    
-    get cookiesAccepted() {
-        return localStorage.getItem(this.storageKey);
-    }
+	}
 
-    hidePopup() {
-        localStorage.setItem(this.storageKey, 'true');
-        this.$element.classList.remove('is-visible');
-        this.$element.setAttribute('aria-hidden', 'true');
-    }
-
-    showPopup() {
-        this.$element.classList.add('is-visible');
-		this.$element.setAttribute('aria-hidden', 'false');
-    }
-
+	@bind('storage', window as any)
 	statusCheck() {
-		if(!this.cookiesAccepted) {
-			this.showPopup()
+		if (!this.cookiesAccepted) {
+			this.showPopup();
 		} else {
 			this.hidePopup();
 		}
-    }
-    
-    @bind('click', '.cookies__button')
-    clsButtonClick() {
-        this.hidePopup();
-    }
+	}
 
-	bind() {
-		this.$on('storage', window, this.statusCheck);
-    }
-    
-    destroy() {
-        this.$off('storage', window, this.statusCheck);
-    }
+	@bind('click', '.cookies__button')
+	onClick() {
+		this.hidePopup();
+	}
+
+	showPopup() {
+		this.$element.classList.add('is-visible');
+		this.$element.setAttribute('aria-hidden', 'false');
+	}
+
+	hidePopup() {
+		localStorage.setItem(this.storageKey, 'true');
+		this.$element.classList.remove('is-visible');
+		this.$element.setAttribute('aria-hidden', 'true');
+	}
 }
